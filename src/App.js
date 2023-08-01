591396b9e84f55251bddce2c2aae7a76d9b3eadc
Daria Piotrowska <daria_piotrowsk@interia.pl>
2023-08-01 18:19:51 +0200

    removed not needed files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+// 0ba05d0772c0df63f36bf3843a553887
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <div></div>;
 }
 
 export default App;
