467980694eae65ab0ca32435f88277816d95a58e
Daria Piotrowska <daria_piotrowsk@interia.pl>
2023-08-01 18:51:48 +0200

    created routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 // 0ba05d0772c0df63f36bf3843a553887
 
 function App() {
-	return <div></div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/movie" element={<MoviePage />} />
+				<Route path="/actor" element={<ActorPage />} />
+				<Route path="/faq" element={<FAQPage />} />
+				<Route path="/rules" element={<RulesPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
