e1a93bddacef09f41f09bcba0fe7193955a89a55
unknown <poczatek.krzysztof@gmail.com>
2023-08-01 19:51:34 +0200

    pagerule

---
+++ src/components/rulescomponents/rulescompoents.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const rulescompoents = () => {
+  return (
+    <div>
+      <h1>Rulez pages</h1>
+      <b>
+      Cel strony: Strona ma na celu dostarczenie informacji i rozrywki związanej z filmami.
+
+Prawa autorskie: Wszystkie treści, takie jak tekst, grafika, zdjęcia i filmy, są chronione prawem autorskim i nie mogą być kopiowane ani używane bez zgody właściciela strony.
+
+Wiek użytkowników: Strona może zawierać treści przeznaczone dla dorosłych lub wymagające określonego wieku użytkownika. Użytkownicy powinni mieć odpowiednią zgodę rodziców lub opiekunów, jeśli są niepełnoletni.
+
+Komunikacja i zachowanie: Wszystkie użytkownicy strony powinni zachowywać się grzecznie i szanować innych użytkowników. Nie jest dozwolone obrażanie, zastraszanie ani publikowanie treści obraźliwych lub nieodpowiednich.
+
+Rejestracja: Jeśli strona oferuje opcję rejestracji konta użytkownika, użytkownicy powinni podać prawdziwe informacje i nie udostępniać swojego hasła innym osobom.
+
+Recenzje i komentarze: Użytkownicy mogą publikować recenzje i komentarze na stronie, ale nie mogą używać ich do spamowania, reklamowania innych stron lub promowania nielegalnych treści.
+
+Bezpieczeństwo: Strona podejmuje wszelkie możliwe środki bezpieczeństwa, ale użytkownicy powinni zachować ostrożność podczas udostępniania swoich danych osobowych lub informacji finansowych.
+
+Cookies: Strona może używać plików cookies w celu śledzenia ruchu na stronie i poprawy użytkowania. Użytkownicy mogą zaakceptować lub odrzucić cookies w ustawieniach swojej przeglądarki.
+
+Zawartość zewnętrzna: Strona może zawierać linki do innych stron internetowych lub treści zewnętrznych. Właściciel strony nie ponosi odpowiedzialności za treści na zewnętrznych stronach.
+
+Zmiany w zasadach: Właściciel strony zastrzega sobie prawo do zmiany zasad w dowolnym momencie. Użytkownicy powinni regularnie sprawdzać zaktualizowane zasady.
+      </b>
+    </div>
+  )
+}
+
+export default rulescompoents
