ccd778fac57ddc211d848311d543a7eb2057da75
unknown <poczatek.krzysztof@gmail.com>
2023-08-10 05:37:33 +0200

    updateRules

--- src/pages/RulesPage.styles.js
+++ src/pages/RulesPage.styles.js
@@ -1,18 +1,14 @@
 import { styled } from 'styled-components';
 
 export const Main = styled.main`
-	max-width: 1400px;
-	font-family: Arial, sans-serif;
 	border: 1px solid #ccc;
 	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 	max-width: 800px;
 	margin: 20px auto;
 	padding: 20px;
-	background-color: #fff;
 `;
 export const H2 = styled.h2`
 	text-align: center;
-	color: #333;
 	margin-bottom: 5px;
 	margin-top: 5px;
 `;
@@ -20,7 +16,6 @@ export const Li = styled.li`
 	margin-left: 30px;
 `;
 export const H3 = styled.p`
-	color: #333;
 	margin-bottom: 5px;
 	margin-top: 5px;
 `;
